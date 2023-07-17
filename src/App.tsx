10bba946aca013f5fc44abdebf93b72bcc8da29f
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-07-17 19:57:00 +0530

    eslint and tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const Home = React.lazy(() => import('./components/Main/Home'))
 
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/challenge/:challenge-name" element="" /> 
+        </Routes>
+      </Router>
     </div>
   );
 }
