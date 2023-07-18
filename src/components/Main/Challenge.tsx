f0b89a06df5d9aa6b249b7da02a14a0e9f3e7d3a
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-07-18 19:21:46 +0530

    accessing compoonent folder

--- src/components/Main/Challenge.tsx
+++ src/components/Main/Challenge.tsx
@@ -1,15 +1,23 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 
 const Header = React.lazy(() => import('../Includes/Header'))
 
+const Counter = React.lazy(() => import('../Challenges/Counter/Counter'))
+
 const Challenge: React.FC = () => {
 
-    const slug = window.location.pathname.trim().split('/')[2]
+    const[slug, setSlug] = useState<string>('')
+
+    useEffect(() => {
+        const slug = window.location.pathname.trim().split('/')[2]
+        setSlug(slug)
+    }, [])
 
     return (
         <div>
             <Suspense fallback={<div></div>}>
                 <Header />
+                {slug === 'counter' && <Counter />}
             </Suspense>
         </div>
     )
